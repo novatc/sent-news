57d2a54b4529b49ead7640d2d74eb1e17efd4c19
novatc <62182386+novatc@users.noreply.github.com>
2022-07-26 19:24:27 +0200

    update code for every argument case

--- send_news.py
+++ send_news.py
@@ -3,7 +3,8 @@ import logging
 import sys
 
 from pandas import read_csv
-from transformers import pipeline, AutoModelForSequenceClassification, DistilBertTokenizer
+from transformers import pipeline, DistilBertTokenizer, \
+    DistilBertForSequenceClassification
 
 from news_api import get_recent_headlines, json_to_dataframe, get_headlines_to_certain_category
 
@@ -19,15 +20,7 @@ def row_to_list(file, row):
     return dict(zip(items, ['None'] * len(items)))
 
 
-# def get_sentiment(model, articles):
-#     # iterate over dict and get sentiment for each article
-#     for key in articles:
-#         articles[key] = model(key)[0]['label']
-#     return articles
-
-
-def get_news(key,category):
-    # not null check
+def get_news(key, category):
     recent_news = get_recent_headlines(key=key)
     logging.info('Request status: {}'.format(recent_news['status']))
     logging.info(f'Fetched {recent_news["totalResults"]} new entries')
@@ -37,12 +30,14 @@ def get_news(key,category):
     recent_news = recent_news.dropna()
 
     if category is not None:
+        logging.info('Fetching headlines for category: {}'.format(category))
         category_news = get_headlines_to_certain_category(key=key, category=category)
         category_news = json_to_dataframe(category_news['articles'])
         category_news = category_news.dropna()
         return recent_news, category_news
 
     return recent_news
+
 
 def get_sentiment(model, articles_dataframe):
     articles_dataframe['sentiment'] = 'None'
@@ -52,6 +47,7 @@ def get_sentiment(model, articles_dataframe):
 
     return articles_dataframe
 
+
 def get_emotion(model, articles_dataframe):
     articles_dataframe['emotion'] = 'None'
 
@@ -59,6 +55,21 @@ def get_emotion(model, articles_dataframe):
         articles_dataframe.at[index, 'emotion'] = model(row['description'])[0]['label']
 
     return articles_dataframe
+
+
+def write_to_csv(dataframe_general, dataframe_category):
+    # drop columns that are not needed
+    dataframe_general = dataframe_general.drop(
+        columns=['urlToImage', 'publishedAt', 'source.id', 'author', 'content', 'url'])
+
+    # save data to csv
+    logging.info('Saving data to csv...')
+
+    dataframe_general.to_csv('output/general_news.csv')
+    if dataframe_category is not None:
+        dataframe_category.drop(
+            columns=['urlToImage', 'publishedAt', 'source.id', 'author', 'content', 'url'])
+        dataframe_category.to_csv('output/category_news.csv')
 
 
 def send_news():
@@ -75,74 +86,84 @@ def send_news():
 
     args = parser.parse_args()
 
+    bin_model_path = 'distilbert-base-uncased-finetuned-sst-2-english'
+    emotion_model_path = 'bhadresh-savani/distilbert-base-uncased-emotion'
 
     logging.info('Loading the model...')
-    if (args.model == 'emotion'):
-        logging.info('Preparing the emotion model...')
-        model = AutoModelForSequenceClassification.from_pretrained("model/emotion")
-        tokenizer = DistilBertTokenizer.from_pretrained("model/vocab.txt")
-        model = prepare_model(model, tokenizer)
 
-    elif (args.model == 'binary'):
-        logging.info('Preparing the binary model...')
-        model = AutoModelForSequenceClassification.from_pretrained("model/binary/500")
-        tokenizer = DistilBertTokenizer.from_pretrained("model/vocab.txt")
-        model = prepare_model(model, tokenizer)
-
-    elif (args.model == 'both'):
+    if (args.category is not None) and (args.model == 'both'):
         logging.info('Preparing both model...')
-        model = AutoModelForSequenceClassification.from_pretrained("model/binary/500")
-        tokenizer = DistilBertTokenizer.from_pretrained("model/vocab.txt")
-        bin_model = prepare_model(model, tokenizer)
-        model = AutoModelForSequenceClassification.from_pretrained("model/emotion")
-        tokenizer = DistilBertTokenizer.from_pretrained("model/vocab.txt")
-        emo_model = prepare_model(model, tokenizer)
+        tokenizer = DistilBertTokenizer.from_pretrained(bin_model_path)
+        bin_model = DistilBertForSequenceClassification.from_pretrained(bin_model_path)
+        bin_model = prepare_model(bin_model, tokenizer)
 
-    if(args.category is not None):
+        emo_model = pipeline("text-classification", model=emotion_model_path,
+                             return_all_scores=False)
+
         general, category_news = get_news(key=args.key, category=args.category)
+        general = get_sentiment(bin_model, general)
+        category_news = get_sentiment(bin_model, category_news)
 
+        general = get_emotion(emo_model, general)
+        category_news = get_emotion(emo_model, category_news)
+
+        write_to_csv(general, category_news)
+
+    if (args.category is None) and (args.model == 'both'):
+        logging.info('Preparing both model...')
+        tokenizer = DistilBertTokenizer.from_pretrained(bin_model_path)
+        bin_model = DistilBertForSequenceClassification.from_pretrained(bin_model_path)
+        bin_model = prepare_model(bin_model, tokenizer)
+
+        emo_model = pipeline("text-classification", model=emotion_model_path,
+                             return_all_scores=False)
+
+        general = get_news(key=args.key, category=None)
         general = get_sentiment(bin_model, general)
         general = get_emotion(emo_model, general)
 
+        write_to_csv(general, None)
+
+    if (args.category is not None) and (args.model == 'binary'):
+        logging.info('Preparing binary model...')
+        tokenizer = DistilBertTokenizer.from_pretrained(bin_model_path)
+        bin_model = DistilBertForSequenceClassification.from_pretrained(bin_model_path)
+        bin_model = prepare_model(bin_model, tokenizer)
+
+        general, category_news = get_news(key=args.key, category=args.category)
+        general = get_sentiment(bin_model, general)
         category_news = get_sentiment(bin_model, category_news)
+        write_to_csv(general, category_news)
+
+    if (args.category is None) and (args.model == 'binary'):
+        logging.info('Preparing binary model...')
+        tokenizer = DistilBertTokenizer.from_pretrained(bin_model_path)
+        bin_model = DistilBertForSequenceClassification.from_pretrained(bin_model_path)
+        bin_model = prepare_model(bin_model, tokenizer)
+
+        general = get_news(key=args.key, category=None)
+        general = get_sentiment(bin_model, general)
+        write_to_csv(general, None)
+
+    if (args.category is not None) and (args.model == 'emotional'):
+        logging.info('Preparing emotional model...')
+        emo_model = pipeline("text-classification", model=emotion_model_path,
+                             return_all_scores=False)
+
+        general, category_news = get_news(key=args.key, category=args.category)
+        general = get_emotion(emo_model, general)
         category_news = get_emotion(emo_model, category_news)
+        write_to_csv(general, category_news)
 
-        #drop columns that are not needed
-        general = general.drop(columns=['urlToImage'])
+    if (args.category is None) and (args.model == 'emotional'):
+        logging.info('Preparing emotional model...')
+        emo_model = pipeline("text-classification", model=emotion_model_path,
+                             return_all_scores=False)
 
-        #save data to csv
-        logging.info('Saving data to csv...')
+        general = get_news(key=args.key, category=None)
+        general = get_emotion(emo_model, general)
+        write_to_csv(general, None)
 
-        general.to_csv('output/general_news.csv')
-        category_news.to_csv('output/category_news.csv')
-
-
-
-
-    # articles = row_to_list(row='description', file='news.csv')
-    #
-    # logging.info('Getting the sentiment...')
-    # get_sentiment(model, articles)
-    #
-    #
-    # logging.info('Saving the sentiment...')
-    # # save dict as csv file
-    # with open('sentiment_pos_neg.csv', 'w') as f:
-    #     w = csv.writer(f)
-    #     w.writerow(['description', 'sentiment'])
-    #     for key, value in articles.items():
-    #         w.writerow([key, value])
-    #
-    # # count positive and negative news
-    # pos_count = 0
-    # neg_count = 0
-    # for key, value in articles.items():
-    #     if value == 'POSITIVE':
-    #         pos_count += 1
-    #     elif value == 'NEGATIVE':
-    #         neg_count += 1
-    # logging.info('Positive news: ' + str(pos_count))
-    # logging.info('Negative news: ' + str(neg_count))
 
 if __name__ == "__main__":
     sys.exit(send_news())
