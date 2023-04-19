31a69a39a9d3dd9595d068786ca1b63c974032e9
Lucas <lcs.wgnr@posteo.de>
2023-04-19 19:06:53 +0200

    stripe endpoint

--- chatGPT_api.py
+++ chatGPT_api.py
@@ -1,54 +1,63 @@
 import openai
 
 # Replace with your own API key
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def analyze_sentiment(article_body):
-    openai.api_key = "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"
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Analyze the sentiment of the following text: \"{article_body}\". "
-               f"Return either Positive, Negative, Critical.",
-        max_tokens=10,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+    openai.api_key = "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"
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"Analyze the sentiment of the following text regarding our future on this planet: \"{article_body}\". "
+                   f"Return either Positive, Negative, Critical.",
+            max_tokens=10,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
 
-    sentiment = response.choices[0].text.strip()
+        sentiment = response.choices[0].text.strip()
+    except:
+        return "no more credits"
     return sentiment
 
 
 def analyze_emotions(article_body):
-    openai.api_key = "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"
+    openai.api_key = "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"
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Please assign two of the following emotions (happiness, sadness, anger, fear, surprise, disgust) "
-               f"in the following text based on their prevalence: \"{article_body}\". "
-               f"Answer with two words starting with a capital letter, no '.' at the end and seperated by a ','.",
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"Please assign two of the following emotions (happiness, sadness, anger, fear, surprise, disgust) "
+                   f"in the following text based on their prevalence: \"{article_body}\" regarding our future on this planet. "
+                   f"Answer with two words starting with a capital letter, no '.' at the end and seperated by a ','.",
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
 
-    ranked_emotions = response.choices[0].text.strip()
+        ranked_emotions = response.choices[0].text.strip()
+    except:
+        return "no more credits"
     return ranked_emotions
 
 
 def summarize_text(article_body, num_sentences=3):
-    openai.api_key = "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"
+    openai.api_key = "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"
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Please summarize the following text in {num_sentences} sentences: \"{article_body}\"",
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"Please summarize the following text in {num_sentences} sentences: \"{article_body}\"",
+            max_tokens=150,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
 
-    summary = response.choices[0].text.strip()
+        summary = response.choices[0].text.strip()
+    except:
+        return "no more credits"
     return summary
---
+++ stripe_backend.py
@@ -0,0 +1,53 @@
+
+import stripe
+
+from flask import Flask, jsonify, request
+
+# The library needs to be configured with your account's secret key.
+# Ensure the key is kept out of any version control system you might be using.
+stripe.api_key = ""
+
+# This is your Stripe CLI webhook secret for testing your endpoint locally.
+endpoint_secret = 'whsec_35509da56bf519778db27f43a3f4dc046788da4c08da66c1e0dd217bd89a3ba2'
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    event = None
+    payload = request.data
+    sig_header = request.headers['STRIPE_SIGNATURE']
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        raise e
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        raise e
+    print(event['type'])
+
+    # Handle the event
+    if event['type'] == 'checkout.session.async_payment_failed':
+      session = event['data']['object']
+    elif event['type'] == 'checkout.session.async_payment_succeeded':
+      session = event['data']['object']
+    elif event['type'] == 'checkout.session.completed':
+      session = event['data']['object']
+    elif event['type'] == 'checkout.session.expired':
+      session = event['data']['object']
+    elif event['type'] == 'customer.created':
+      customer = event['data']['object']
+    elif event['type'] == 'customer.deleted':
+      customer = event['data']['object']
+    # ... handle other event types
+    else:
+      print('Unhandled event type {}'.format(event['type']))
+
+    return jsonify(success=True)
+
+if __name__ == '__main__':
+    app.run(port=4242)
